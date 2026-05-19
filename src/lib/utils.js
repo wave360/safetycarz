6fba9eef62b71594ed1fbd8db1a31af0073b9562
wave360 <wassimabouri.wa@gmail.com>
2026-05-19 23:37:06 +0200

    ajout

---
+++ src/lib/utils.js
@@ -0,0 +1,19 @@
+export const formatDate = (dateString) => {
+  const date = new Date(dateString)
+  return date.toLocaleDateString('fr-FR', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+  })
+}
+
+export const slugify = (text) => {
+  return text
+    .toString()
+    .toLowerCase()
+    .replace(/\s+/g, '-')
+    .replace(/[^\w-]+/g, '')
+    .replace(/--+/g, '-')
+    .replace(/^-+/, '')
+    .replace(/-+$/, '')
+}
